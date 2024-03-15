469c0dadd31520e00a58d078890a9ed8994df20c
Pete Cheslock <petecheslock@gmail.com>
2024-03-15 16:12:29 -0400

    Remove appmap from the frontend and fix typo

--- doit.sh
+++ doit.sh
@@ -2,14 +2,16 @@
 # Install with asdf https://github.com/particledecay/asdf-task
 # or install direct https://taskfile.dev/#/installation
 
+# setup
 task setup
 
+# run container dependencies
 task dev:services
 
 # Terminal #1
 task py:postgres
 
-# Terminal #3
+# Terminal #2
 task ui
 
 # or run tests
